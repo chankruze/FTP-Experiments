5a1f87651763beb17862c359e1edbce0e840c4d6
chankruze <chankruze@gmail.com>
2019-10-22 04:36:45 +0530

    Show connection status to user
    Signed-off-by: chankruze <chankruze@gmail.com>

--- app/src/main/java/in/geekofia/ftpfm/activities/FilesActivity.java
+++ app/src/main/java/in/geekofia/ftpfm/activities/FilesActivity.java
@@ -8,13 +8,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.TabHost;
+import android.widget.TextView;
 
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -32,11 +33,16 @@ public class FilesActivity extends ListActivity {
     List<Item> files = new ArrayList<Item>();
     FTPFile[] currentDir = new FTPFile[0];
     private FileListAdapter adapter;
-    private ListView listView;
     FTPFile[] ftpDirs = new FTPFile[0];
     String path = new String();
     private String host, username, password;
     private int port;
+
+    // Views
+    private ListView mListView;
+    private TextView mErrorText;
+    private ImageView mImageView;
+    private Button mBtnEditConnection;
 
     private String TAG = getClass().getSimpleName();
 
@@ -47,7 +53,7 @@ public class FilesActivity extends ListActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_files);
 
-        listView = findViewById(android.R.id.list);
+        initViews();
 
         Bundle b = getIntent().getExtras();
         ftpclient = b.getParcelable("FTP_CLIENT");
@@ -71,6 +77,14 @@ public class FilesActivity extends ListActivity {
                 if (status == true) {
                     Log.d(TAG, "Connection Success");
 
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mErrorText.setText("Connection Success");
+                            mImageView.setImageDrawable(getDrawable(R.drawable.ic_successful_connection));
+                        }
+                    });
+
                     try {
                         ftpDirs = ftpclient.listDirectories();
                     } catch (IOException e) {
@@ -86,12 +100,12 @@ public class FilesActivity extends ListActivity {
                     for (FTPFile f : ftpDirs) {
                         // is a directory
                         if (f.isDirectory()) {
-                            Item item = new Item(Item.DIRECTORY, f.getName(), f.getSize(), f.getName());
+                            Item item = new Item(Item.DIRECTORY, f.getName(), f.getSize(), f.getName() + "/");
                             directories.add(item);
                         }
                         // is a file
                         else {
-                            Item item = new Item(Item.FILE, f.getName(), f.getSize(), f.getName());
+                            Item item = new Item(Item.FILE, f.getName(), f.getSize(), f.getName() + "/");
                             files.add(item);
                         }
                     }
@@ -106,15 +120,48 @@ public class FilesActivity extends ListActivity {
                         @Override
                         public void run() {
                             adapter = new FileListAdapter(getApplicationContext(), directories);
-                            listView.setAdapter(adapter);
+                            mListView.setAdapter(adapter);
+                            mErrorText.setVisibility(View.GONE);
+                            mImageView.setVisibility(View.GONE);
                         }
                     });
 
                 } else {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            mErrorText.setText("Connection failed");
+                            mErrorText.setVisibility(View.VISIBLE);
+                            mImageView.setImageDrawable(getDrawable(R.drawable.ic_alert));
+                            mImageView.setVisibility(View.VISIBLE);
+                            mListView.setVisibility(View.GONE);
+                            mBtnEditConnection.setVisibility(View.VISIBLE);
+                            mBtnEditConnection.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    finish();
+                                }
+                            });
+                        }
+                    });
+
                     Log.d(TAG, "Connection failed");
                 }
             }
         }).start();
+    }
+
+    private void initViews() {
+        mListView = findViewById(android.R.id.list);
+
+        mErrorText = findViewById(R.id.error_text);
+        mErrorText.setText("Connecting...");
+
+        mImageView = findViewById(R.id.error_logo);
+        mImageView.setImageDrawable(getDrawable(R.drawable.ic_hourglass));
+
+        mBtnEditConnection = findViewById(R.id.btn_edit_details);
+        mBtnEditConnection.setVisibility(View.GONE);
     }
 
     @Override
